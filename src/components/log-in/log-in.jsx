e7b753ffa460ac55a0e0729856a05c155a12b352
redcapwithdirt <redcapwithdirt@gmail.com>
2023-09-07 15:49:09 +0530

    added use state and some functions

--- src/components/log-in/log-in.jsx
+++ src/components/log-in/log-in.jsx
@@ -2,9 +2,30 @@ import './log-in.scss';
 import logo from '../../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
 
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+}
 
 const Login = () => {
+    const [formfield, setFormfield] = useState(defaultFormFields)
+    const { email, password } = formfield
+    const resetFormField = () => {
+        setFormfield(defaultFormFields)
+    }
+
+
+    const handlechange = (event) => {
+        const { name, value } = event.target
+        setFormfield({ ...formfield, [name]: value })
+    }
+
+    const handleSubmit=(event)=>{
+       resetFormField(defaultFormFields)
+    }
     return (
         <div className='login-outer'>
             <div className='login'>
@@ -15,35 +36,39 @@ const Login = () => {
                 <h3 className='enter_email'>Enter your email and password below</h3>
                 <div className='container'>
 
-                    {/* Enter your email */}
-                    <div className='foremail'>
-                        <label for="uname"><b>EMAIL</b></label>
-                        <input type="text" placeholder="Email address" name="uname" required></input>
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        {/* Enter your email */}
+                        <div className='foremail'>
+                            <label><b>EMAIL</b></label>
+                            <input type="email" placeholder="Email address" onChange={handlechange} name="email" value={email} required ></input>
+                        </div>
 
 
-                    {/* Enter your password */}
-                    <div className='forpass'>
-                        <label for="psw"><b>Password</b></label>
-                        <a href="/">Forgot password?</a>
-                        <input type="password" placeholder="Enter Password" name="psw" required></input>
-                    </div>
+                        {/* Enter your password */}
+                        <div className='forpass'>
+                            <label><b>Password</b></label>
+                            <a href="/">Forgot password?</a>
+                            <input type="password" placeholder="Enter Password" onChange={handlechange} name="password" value={password} required></input>
+                        </div>
 
-                    {/* login button  */}
-                    <div className='button-container'>
-                        <button className="butt">
-                            Log in
-                        </button>
+                        {/* login button  */}
+                        <div className='button-container'>
+                            <button type='submit' className="butt">
+                                Log in
+                            </button>
 
-                    </div>
+                        </div>
 
-                    {/* google sign in button */}
-                    <div className='googlesignin'>
-                        <p className='another_signin'>or you can log in with</p>
+                        {/* google sign in button */}
+                        <div className='googlesignin'>
+                            <p className='another_signin'>or you can log in with</p>
 
-                        {/* google icon  */}
-                        <button className="google_button" type='button'><FontAwesomeIcon className='google' icon={faGoogle} color="#9FA2B4" /></button>
-                    </div>
+                            {/* google icon  */}
+                            <button className="google_button" type='button'><FontAwesomeIcon className='google' icon={faGoogle} color="#9FA2B4" /></button>
+                        </div>
+                    </form>
+
+
 
                     {/* sign up  */}
                     <div className='sign-up'>
